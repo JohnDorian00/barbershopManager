e92fb16d6e529031bc8722e8657c02ae06981475
JohnDorian00 <32276068+JohnDorian00@users.noreply.github.com>
2021-06-14 20:45:11 +0300

    upd

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,23 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+import git
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asdasfsgdf'
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/JohnDorian00/barbershopManager')
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 @app.route('/')
